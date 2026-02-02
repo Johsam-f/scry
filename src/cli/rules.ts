b65ab739e8108059c08feb7ced82eeabd935e030
Johsam <johsamfcress@gmail.com>
2026-02-02 21:03:26 +0200

    feat: add CORSConfig, EnvExposure, and WeakCrypto rules to the rule registry

--- src/cli/rules.ts
+++ src/cli/rules.ts
@@ -5,13 +5,19 @@ import { HardcodedSecretsRule } from '../rules/hardcodedSecrets';
 import { JWTStorageRule } from '../rules/jwtStorage';
 import { EvalUsageRule } from '../rules/evalUsage';
 import { CookieSecurityRule } from '../rules/cookieSecurity';
+import { CORSConfigRule } from '../rules/corsConfig';
+import { EnvExposureRule } from '../rules/envExposure';
+import { WeakCryptoRule } from '../rules/weakCrypto';
 
 export function getAllRules(): Rule[] {
   return [
     new HardcodedSecretsRule(),
     new JWTStorageRule(),
     new EvalUsageRule(),
-    new CookieSecurityRule()
+    new CookieSecurityRule(),
+    new CORSConfigRule(),
+    new EnvExposureRule(),
+    new WeakCryptoRule()
   ];
 }
 
